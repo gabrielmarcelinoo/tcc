90241bf850d21c9883a91179e184b8e46d07cc0b
gabrielmarcelinoo <gabriel.marcelino.1302@gmail.com>
2023-08-24 11:48:10 -0300

    arrumando layout site

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "img/menu-aberto.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+        document.querySelector('.icon').src = "img/x (1).png";
     }
 }
